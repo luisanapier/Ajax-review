b1bc5f1c59aa1c98bdcf0bcc85511610b97b1d3e
Luisa Napier <luisanapier@Luisas-MacBook-Pro.local>
2023-07-20 20:30:28 -0700

    AJAX homework completed

---
+++ static/ajax-review.js
@@ -0,0 +1,26 @@
+// PART 1: SHOW A FORTUNE
+
+function showFortune() {
+  
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((responseData) => {
+      document.querySelector('#fortune-text').innerHTML = responseData;
+    });
+}
+document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+  evt.preventDefault();
+  const zipcode = document.querySelector('#zipcode-field').value;
+  const url = `/weather?zipcode=${zipcode}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      document.querySelector('#weather-info').innerText = responseJson.forecast;
+    }); 
+}
+ 
+document.querySelector('#weather-form').addEventListener('submit', showWeather);
